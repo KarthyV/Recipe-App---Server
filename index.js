3e5c7d8683e2751dc071258f7e20ef3ed2dba558
KarthyV <Kdon979@gmail.com>
2022-11-18 14:50:18 +0530

    database connected

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+
+app.use(express.json()); // middleware for converting data to JSON
+
+await mongoose.connect(process.env.MONGO_URL);
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-const PORT = 8000;
+const PORT = 8000 || process.env.PORT;
 app.listen(PORT, () => {
   console.log("listening on port " + PORT);
 });
