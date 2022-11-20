79d81f67482546a1ef949d8690049fe49f87da0e
KarthyV <Kdon979@gmail.com>
2022-11-20 18:28:02 +0530

    cors added

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import recipeRoutes from "./routes/recipe.routes.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.json()); // middleware for converting data to JSON
 
 await mongoose.connect(process.env.MONGO_URL);
